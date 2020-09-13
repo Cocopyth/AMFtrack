7f3ee1280bed9ee867db2279715f05d7021d63d4
Cocopyth <corentinbees@gmail.com>
2020-09-13 23:15:08 +0200

    worked on node identification

--- plotutil.py
+++ plotutil.py
@@ -37,8 +37,21 @@ def show_im_rgb(matrix):
 
 def overlap(skel,raw):
     kernel = np.ones((3,3),np.uint8)
-    dilated = cv2.dilate(skel,kernel,iterations = 4)
+#     dilated = cv2.dilate(skel,kernel,iterations = 4)
     fig = plt.figure()
-    plt.imshow(raw, cmap='gray',interpolation='none')
-    plt.imshow(dilated, cmap='jet', alpha=0.5,interpolation='none')
+    matrix=skel
+    ax = fig.add_subplot(111)
+    ax.imshow(raw, cmap='gray',interpolation='none')
+    ax.imshow(skel, cmap='jet', alpha=0.5,interpolation='none')
+    numrows, numcols = matrix.shape[0],matrix.shape[1]
+    def format_coord(x, y):
+        col = int(x+0.5)
+        row = int(y+0.5)
+        if col>=0 and col<numcols and row>=0 and row<numrows:
+            z = matrix[row,col]
+            return 'x=%1.4f, y=%1.4f, z=%1.4f'%(y, x, z)
+        else:
+            return 'x=%1.4f, y=%1.4f'%(y, x)
+
+    ax.format_coord = format_coord
     plt.show()
