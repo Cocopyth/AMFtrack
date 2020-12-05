d333f86a24baefe747a3d6fb1748208b8c655874
Mathijs Rozemuller <mrozemul@int2.bullx>
2020-12-05 15:01:00 +0100

    working on extracting skel

---
+++ extract_skel_surf.py
@@ -0,0 +1,85 @@
+from util import get_path
+import pandas as pd
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+from extract_graph import generate_nx_graph, transform_list, generate_skeleton, generate_nx_graph_from_skeleton, from_connection_tab, from_nx_to_tab
+from node_id import whole_movement_identification, second_identification
+import ast
+from plotutil import plot_t_tp1, compress_skeleton
+from scipy import sparse
+from sparse_util import dilate, zhangSuen
+from realign import realign
+from datetime import datetime,timedelta
+from node_id import orient
+import pickle
+from matplotlib.widgets import CheckButtons
+import scipy.io as sio
+import imageio
+from pymatreader import read_mat
+from matplotlib import colors
+from copy import deepcopy,copy
+from collections import Counter
+import cv2 
+import imageio
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.filters import frangi
+from skimage.morphology import thin
+from skimage import data, filters
+from random import choice
+import scipy.sparse
+import os
+from time import time
+date = '0714_1534'
+date_plate = f'/2020{date}'
+plate = 9
+plate_str = f'_Plate0{plate}'
+path_snap='/scratch/shared/mrozemul/Fiji.app/'+date_plate+plate_str
+path_tile=path_snap+'/Img/TileConfiguration.txt.registered'
+tileconfig = pd.read_table(path_tile,sep=';',skiprows=4,header=None,converters={2 : ast.literal_eval},skipinitialspace=True)
+dirName = path_snap+'/Analysis'
+shape = (3000,4096)
+try:
+    os.mkdir(path_snap+'/Analysis') 
+    print("Directory " , dirName ,  " Created ")
+except FileExistsError:
+    print("Directory " , dirName ,  " already exists")  
+t=time()
+xs =[c[0] for c in tileconfig[2]]
+ys =[c[1] for c in tileconfig[2]]
+dim = (int(np.max(ys)-np.min(ys))+4096,int(np.max(xs)-np.min(xs))+4096)
+ims = []
+for name in tileconfig[0]:
+#     ims.append(imageio.imread('//sun.amolf.nl/shimizu-data/home-folder/oyartegalvez/Drive_AMFtopology/PRINCE'+date_plate+plate_str+'/Img/'+name))
+    ims.append(imageio.imread(f'{name}'))
+skel = np.zeros(dim,dtype=np.uint8)
+for index,im in enumerate(ims):
+    print(index)
+    im_cropped = im
+    im_blurred =cv2.blur(im_cropped, (200, 200))
+    im_back_rem = (im_cropped+1)/(im_blurred+1)
+    im_back_rem = cv2.normalize(im_back_rem, None, 0, 255, cv2.NORM_MINMAX)
+    im_back_rem_inv = cv2.normalize(255-im_back_rem, None, 0, 255, cv2.NORM_MINMAX)
+    frangised = frangi(im_back_rem,sigmas=range(1,20,4))
+    frangised = cv2.normalize(frangised, None, 0, 255, cv2.NORM_MINMAX)
+    transformed = cv2.normalize(frangised-im_back_rem, None, 0, 255, cv2.NORM_MINMAX)
+    transformed = cv2.normalize(-im_back_rem, None, 0, 255, cv2.NORM_MINMAX)
+    low = 180
+    high = 200
+    lowt = (transformed > low).astype(int)
+    hight = (transformed > high).astype(int)
+    hyst = filters.apply_hysteresis_threshold(transformed, low, high)
+    kernel = np.ones((3,3),np.uint8)
+    dilation = cv2.dilate(hyst.astype(np.uint8) * 255,kernel,iterations = 1)
+    for i in range(3):
+        dilation=cv2.erode(dilation.astype(np.uint8) * 255,kernel,iterations = 1)
+        dilation = cv2.dilate(dilation.astype(np.uint8) * 255,kernel,iterations = 1)
+    dilated = dilation>0
+#     np.save(f'Temp\dilated{tileconfig[0][i]}',dilated)
+    boundaries = int(tileconfig[2][index][0]-np.min(xs)),int(tileconfig[2][index][1]-np.min(ys))
+    skel[boundaries[1]:boundaries[1]+shape[0],boundaries[0]:boundaries[0]+shape[1]] += dilated
+skeletonized = cv2.ximgproc.thinning(np.array(255*(skel>0),dtype=np.uint8))
+sio.savemat(path_snap+'/Analysis/dilated.mat',{'dilated' : skel})
+sio.savemat(path_snap+'/Analysis/skeleton.mat',{'skeleton' : skeletonized})
+print('time=',time()-t)
