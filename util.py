5b189fd55f1689855eb440e3f0af66023c2dae77
Corentin <Corentin@bisot.comx>
2021-01-07 12:59:16 +0100

    Improving data pipeline for easier use

--- util.py
+++ util.py
@@ -1,5 +1,7 @@
 from scipy import sparse
 import numpy as np
+import os
+from datetime import datetime,timedelta
 
 def get_path(date,plate,skeleton,row=None,column=None,extension=".mat"):
     def get_number(number):
@@ -15,6 +17,15 @@ def get_path(date,plate,skeleton,row=None,column=None,extension=".mat"):
     else:
         end='/Img'+f'/Img_r{get_number(row)}_c{get_number(column)}.tif'
     return (root_path+date_plate+plate+end)
+
+def get_dates_datetime(directory,plate):
+    listdir=os.listdir(directory) 
+    list_dir_interest=[name for name in listdir if name.split('_')[-1]==f'Plate{0 if plate<10 else ""}{plate}']
+    ss=[name.split('_')[0] for name in list_dir_interest]
+    ff=[name.split('_')[1] for name in list_dir_interest]
+    dates_datetime=[datetime(year=int(ss[i][:4]),month=int(ss[i][4:6]),day=int(ss[i][6:8]),hour=int(ff[i][0:2]),minute=int(ff[i][2:4])) for i in range(len(list_dir_interest))]
+    dates_datetime.sort()
+    return(dates_datetime)
 
 def shift_skeleton(skeleton,shift):
     shifted_skeleton=sparse.dok_matrix(skeleton.shape, dtype=bool)
