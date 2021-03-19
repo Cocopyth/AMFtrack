163e517a8aae3f402f9be641acc8722bdc54069b
Corentin Bisot <cbisot@int2.bullx>
2021-03-19 16:47:17 +0100

    Updated analysis

--- sample/notebooks/analysis/extract_bas.py
+++ sample/notebooks/analysis/extract_bas.py
@@ -1,10 +1,10 @@
 import sys  
 sys.path.insert(0, '/home/cbisot/pycode/MscThesis/')
 from sample.notebooks.analysis.util import *
-from sample.paths.directory import path_code
+from sample.paths.directory import path_code, directory_scratch, directory_project
 from sample.notebooks.analysis.data_info import *
 
 for treatment in treatments.keys():
     insts = treatments[treatment]
-    bas_frequs = estimate_bas_freq_mult(insts,1000,0,criter)
+    bas_frequs = estimate_bas_freq_mult(insts,1000,0,criter, directory_project)
     pickle.dump(bas_frequs, open(f'{path_code}/MscThesis/Results/bas_{treatment}.pick', "wb"))
