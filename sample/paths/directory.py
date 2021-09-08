900c55f681fa1c3914c43b4651f0c1dc2e8bcc0d
Corentin Bisot <cbisot@int2.bullx>
2021-09-08 06:51:59 +0200

    first post holiday comit, saving before moving to snelli, woorking version of stitching

--- sample/paths/directory.py
+++ sample/paths/directory.py
@@ -52,6 +52,45 @@ def check_state(plate,begin,end,file,directory):
             not_exist.append((date,i+begin))
     return(not_exist)
 
+def make_stitching_loop(directory,dirname,index):
+    a_file = open(f'{path_code}MscThesis/sample/pipeline/scripts/stitching_loops/stitching_loop.ijm',"r")
+
+    list_of_lines = a_file.readlines()
+
+    list_of_lines[4] = f'mainDirectory = \u0022{directory}\u0022 ;\n'
+    list_of_lines[29] = f'\t if(startsWith(list[i],\u0022{dirname}\u0022)) \u007b\n'
+    file_name = f'{path_code}MscThesis/sample/pipeline/scripts/stitching_loops/stitching_loop{index}.ijm'
+    a_file = open(file_name, "w")
+
+    a_file.writelines(list_of_lines)
+
+    a_file.close()
+    
+def run_parallel_stitch(plate, directory, begin, end, num_parallel, time):
+    begin_skel = begin // num_parallel
+    end_skel = (end) // num_parallel + 1
+    listdir = os.listdir(directory)
+    list_dir_interest = [name for name in listdir if name.split('_')[-1]==f'Plate{0 if plate<10 else ""}{plate}']
+    dates_datetime = get_dates_datetime(directory,plate)
+    for j in range(begin_skel, end_skel):
+        start = num_parallel * j + begin % num_parallel
+        stop = num_parallel * j + num_parallel + begin % num_parallel
+        for k in range(start,stop):
+            make_stitching_loop(directory,get_dirname(dates_datetime[k], plate),k)
+        ide = int(datetime.now().timestamp())
+        my_file = open(path_job, "w")
+        my_file.write(
+            f"#!/bin/bash \n#Set job requirements \n#SBATCH -N 1 \n#SBATCH -t {time}\n#SBATCH -p normal\n"
+        )
+        my_file.write(
+            f'#SBATCH -o "{path_code}MscThesis/slurm/stitching__{start}_{stop}_{ide}.out" \n'
+        )
+        for k in range(start,stop):
+            my_file.write(f"~/Fiji.app/ImageJ-linux64 --headless -macro  {path_code}MscThesis/sample/pipeline/scripts/stitching_loops/stitching_loop{k}.ijm &\n")
+        my_file.write("wait\n")
+        my_file.close()
+        call(f"sbatch {path_job}", shell=True)
+
 def find_state(plate,begin,end,directory,include_stitch=True):
     files = [ '/Analysis/skeleton_compressed.mat', '/Analysis/skeleton_masked_compressed.mat',
              '/Analysis/skeleton_pruned_compressed.mat', '/Analysis/transform.mat',
