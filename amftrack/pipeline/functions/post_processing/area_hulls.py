60162bc779a3a668e7447b60bb9a4b2a616b8093
Corentin Bisot <corentin@bisot.com>
2021-12-30 12:03:17 +0100

    modifiction after sync with computer

--- amftrack/pipeline/functions/post_processing/area_hulls.py
+++ amftrack/pipeline/functions/post_processing/area_hulls.py
@@ -1,9 +1,13 @@
+import os.path
+
 from shapely.geometry import Polygon, shape,Point
 from scipy import spatial
 import networkx as nx
-from amftrack.pipeline.functions.image_processing.experiment_class_surf import Edge, Node
 from amftrack.pipeline.functions.post_processing.util import get_length_um_edge, is_in_study_zone
+from amftrack.pipeline.paths.directory import directory_scratch
 import numpy as np
+from time import time_ns
+import pickle
 
 def get_hulls(exp,ts):
     hulls = []
@@ -78,26 +82,40 @@ def get_regular_hulls_area_ratio(num,exp,ts):
         regular_hulls.append(hulls[index])
     return(regular_hulls,indexes)
 
-def get_regular_hulls_area_fixed(exp,ts,incr,num):
-    hulls = get_hulls(exp,ts)
-    areas = [hull.area* 1.725**2/(1000**2) for hull in hulls]
-    regular_hulls = [hulls[0]]
-    init_area = areas[0]
-    indexes = [0]
-    current_area = init_area
-    for i in range(num-1):
-        current_area += incr
-        index = min([i for i in range(len(areas)) if areas[i]>=current_area])
-        indexes.append(index)
-        regular_hulls.append(hulls[index])
+def get_regular_hulls_area_fixed(exp,ts,incr):
+    path = directory_scratch + \
+           f"temp/hulls_{exp.plate}_{incr}_" \
+           f"{exp.dates[0].strftime('%m%d%Y%H:%M:%S')}.pick"
+    if os.path.isfile(path):
+        (regular_hulls, indexes) = pickle.load(open(path,"rb"))
+    else:
+        hulls = get_hulls(exp,ts)
+        areas = [hull.area* 1.725**2/(1000**2) for hull in hulls]
+        area_incr = areas[-1] - areas[0]
+        num = int(area_incr / incr)
+        regular_hulls = [hulls[0]]
+        init_area = areas[0]
+        indexes = [0]
+        current_area = init_area
+        for i in range(num-1):
+            current_area += incr
+            index = min([i for i in range(len(areas)) if areas[i]>=current_area])
+            indexes.append(index)
+            regular_hulls.append(hulls[index])
+
+        pickle.dump((regular_hulls,indexes), open(path,"wb"))
     return(regular_hulls,indexes)
 
 
 def get_density_in_ring(exp,t,args):
+    print(t)
     incr = args["incr"]
     i = args["i"]
-    regular_hulls,indexes = get_regular_hulls_area_fixed(exp,range(exp.ts),incr,i+2)
-    hull1,hull2 = regular_hulls[i],regular_hulls[i+1]
-    length = get_length_in_ring(hull1,hull2,t,exp)
-    area= ring_area(hull1,hull2)
-    return(f"ring_density_incr-{incr}_index-{i}",length/area)
+    regular_hulls,indexes = get_regular_hulls_area_fixed(exp,range(exp.ts),incr)
+    if i+2<=len(regular_hulls):
+        hull1,hull2 = regular_hulls[i],regular_hulls[i+1]
+        length = get_length_in_ring(hull1,hull2,t,exp)
+        area= ring_area(hull1,hull2)
+        return(f"ring_density_incr-{incr}_index-{i}",length/area)
+    else:
+        return(f"ring_density_incr-{incr}_index-{i}",None)
