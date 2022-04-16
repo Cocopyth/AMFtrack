e23c4745852a4f018dc64ddd02782ecab0a9cdbc
Félix Kahane <felix@kahane.fr>
2022-04-16 20:25:08 +0200

    Refactor the utils

--- amftrack/util/aliases.py
+++ amftrack/util/aliases.py
@@ -2,6 +2,5 @@ from typing import Dict, Tuple
 import numpy as np
 
 node_coord_dict = Dict[str, Tuple[int, int]]
-image = np.array  # array of dimension 2
 binary_image = np.array  # boolean array of dimension 2
 coord = Tuple[float, float]
--- amftrack/util/image_analysis.py
+++ amftrack/util/image_analysis.py
@@ -1,49 +1,44 @@
 import numpy as np
 
 
-def expand_segment(point1, point2, factor):
-    """
-    From the segment [point1, point2], return a new segment dilated by `factor`
-    and centered on the same point.
-    """
-    x1, y1 = point1[0], point1[1]
-    x2, y2 = point2[0], point2[1]
-    vx = x2 - x1
-    vy = y2 - y1
-    x_mid = (x1 + x2) / 2
-    y_mid = (y1 + y2) / 2
-    x1_, y1_ = [x_mid - factor * vx / 2, y_mid - factor * vy / 2]
-    x2_, y2_ = [x_mid + factor * vx / 2, y_mid + factor * vy / 2]
-    return [x1_, y1_], [x2_, y2_]
-
-
-def compute_factor(point1, point2, target_length):
-    """
-    Determine the dilation factor to reach the target_length
-    """
-    x1, y1 = point1[0], point1[1]
-    x2, y2 = point2[0], point2[1]
-    vx = x2 - x1
-    vy = y2 - y1
-    length = np.sqrt(vx**2 + vy**2)
-
-    return target_length / length
-
-
 def convert_to_micrometer(pixel_length, magnification=2):
+    """
+    Converts pixels into micrometers, based on the magnification of the microscope.
+    """
     camera_res = 3.45
     return pixel_length * camera_res / magnification
 
 
+def find_transformation(old_coord_list: List, new_coord_list: List):
+    """
+    Compute the rotation and translation to transform the old plane into the new one.
+    old_coord_list and new_coord_list must contain at least 2 points.
+    Ex:
+    find_transformation([[16420,26260],[17120, 28480]], [[15760, 26500],[16420, 28780]])
+    :return: Two arrays: a rotation and a translation
+    """
+
+    H = np.dot(
+        np.transpose(np.array(old_coord_list) - np.mean(old_coord_list, axis=0)),
+        np.array(new_coord_list) - np.mean(new_coord_list, axis=0),
+    )
+
+    U, S, V = np.linalg.svd(H)
+    R = np.dot(V, np.transpose(U))
+    t = np.mean(new_coord_list, axis=0) - np.dot(R, np.mean(old_coord_list, axis=0))
+
+    return R, t
+
+
+def get_transformation(R, t):
+    """Returns the function to apply to the points to apply the transformation"""
+    return lambda x: np.dot(R, x) + t
+
+
 if __name__ == "__main__":
 
-    point1 = [0, 0]
-    point2 = [2, 4]
-    assert expand_segment(point1, point2, 0.5) == ([0.5, 1.0], [1.5, 3.0])
-    assert expand_segment(point1, point2, 2) == ([-1.0, -2.0], [3.0, 6.0])
-    assert expand_segment(point2, point1, 2) == ([3.0, 6.0], [-1.0, -2.0])
-
-    assert compute_factor([0, 0], [0, 3], 6) == 2
-    assert compute_factor([1, 0], [10, 0], 4.5) == 0.5
-
-    assert convert_to_micrometer(10) == 17.25
+    R, t = find_transformation(
+        [[16420, 26260], [17120, 28480]], [[15760, 26500], [16420, 28780]]
+    )
+    f = get_transformation(R, t)
+    print(f([16420, 26260]))
