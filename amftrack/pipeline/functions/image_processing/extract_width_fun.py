28d90338d329bef2fe07bd0c68021da7855adbca
Félix Kahane <felix@kahane.fr>
2022-04-19 18:57:46 +0200

    Add comments and a test

--- amftrack/pipeline/functions/image_processing/extract_width_fun.py
+++ amftrack/pipeline/functions/image_processing/extract_width_fun.py
@@ -1,51 +1,32 @@
 from skimage.measure import profile_line
-from amftrack.notebooks.analysis.util import *
-from scipy.optimize import curve_fit
 import numpy as np
+from scipy import special
+from scipy.optimize import curve_fit
+from typing import Tuple, List
+
+from amftrack.notebooks.analysis.util import *
+from amftrack.util.aliases import coord
 from amftrack.pipeline.functions.image_processing.experiment_class_surf import (
     Experiment,
+    Edge,
 )
 
 a = 2.3196552
-from scipy import special
 
 
-def func2(x, lapse, lapse2, c, d, e):
-    return (
-        -c * (special.erf(e * (x - lapse)) - special.erf(e * (x - lapse - lapse2))) + d
-    )
-
-
-def func3(x, lapse, lapse2, c, d, e, lapse4):
-    return (
-        -c * (special.erf(e * (x - lapse)) - special.erf(e * (x - (lapse + lapse2))))
-        + d
-        + c
-        * (
-            special.erf(e * (x - (lapse + lapse2)))
-            - special.erf(e * (x - (lapse + lapse2 + lapse4)))
-        )
-    )
-
-
-def func4(x, lapse, lapse2, c, d, e, lapse4):
-    return (
-        -c * (special.erf(e * (x - lapse)) - special.erf(e * (x - (lapse + lapse2))))
-        + d
-        + c * (special.erf(e * (x - (lapse - lapse4))) - special.erf(e * (x - (lapse))))
-    )
-
-
-def func5(x, sigma, mean, fact, offset):
-    return -fact * np.exp(-((x - mean) ** 2) / sigma**2) + offset
-
-
-def func5(x, sigma, mean, fact, offset):
-    return -fact * np.exp(-((x - mean) ** 2) / sigma**2) + offset
-
-
-def get_source_image(experiment: Experiment, pos, t, local, force_selection=None):
-
+def get_source_image(
+    experiment: Experiment, pos: coord, t: int, local: bool, force_selection=None
+):
+    """
+    Return a source image from the Experiment object for the position `pos`.
+    If force_selection is None, it returns the image in which the point is
+    further away from the border.
+    Otherwise, it returns the image the closest to the `force_selection` coordinates
+    :param pos: (x,y) position
+    :param local: useless parameter boolean
+    :param force_selection: (x,y) position
+    :return: image along with its coordinates
+    """
     x, y = pos[0], pos[1]
     ims, posimg = experiment.find_image_pos(x, y, t, local)
     if force_selection is None:
@@ -66,29 +47,36 @@ def get_source_image(experiment: Experiment, pos, t, local, force_selection=None
 
 
 def get_width_pixel(
-    edge,
+    edge: Edge,
     index,
     im,
     pivot,
-    before,
-    after,
+    before: coord,
+    after: coord,
     t,
     size=20,
     width_factor=60,
     averaging_size=100,
     threshold_averaging=10,
 ):
+    """
+    Get a width value for a given pixel on the hypha
+    :param index: TO REMOVE
+    :param im:
+    :param pivot:
+    :param before, after: coordinates of point after and before to compute the tangent
+    :param size:
+    :param width_factor:
+    :param averaging_size:
+    :param threshold_averaging:
+    :return: the width computed
+    """
+    # TODO(FK): remove this line
     imtab = im
     #     print(imtab.shape)
     #     print(int(max(0,pivot[0]-averaging_size)),int(pivot[0]+averaging_size))
-    threshold = np.mean(
-        imtab[
-            int(max(0, pivot[0] - averaging_size)) : int(pivot[0] + averaging_size),
-            int(max(0, pivot[1] - averaging_size)) : int(pivot[1] + averaging_size),
-        ]
-        - threshold_averaging
-    )
     orientation = np.array(before) - np.array(after)
+    # TODO(FK): all this into another function
     perpendicular = (
         [1, -orientation[0] / orientation[1]] if orientation[1] != 0 else [0, 1]
     )
@@ -99,42 +87,10 @@ def get_width_pixel(
     point2 = np.around(np.array(pivot) - width_factor * perpendicular_norm)
     point1 = point1.astype(int)
     point2 = point2.astype(int)
-    p = profile_line(imtab, point1, point2, mode="constant")
+    p = profile_line(imtab, point1, point2, mode="constant")  # TODO(FK): solve error
     xdata = np.array(range(len(p)))
     ydata = np.array(p)
-    # #     fig = plt.figure()
-    # #     ax = fig.add_subplot(111)
-    # #     ax.plot(xdata,ydata)
-    # #     ax.plot(xdata, func5(xdata, *popt0), 'g-')
-    #     try:
-    #         p00=[10,60,60,160]
-    #         popt0, pcov = curve_fit(func5, xdata, ydata,bounds = ([0,0,0,0],4*[np.inf]),p0=p00)
-    #         p0a=[60,10,100,180,0.1]
-    #         popt1, pcov = curve_fit(func2, xdata, ydata,bounds = ([0,0,0,0,0],[120,120,200]+2*[np.inf]),p0=p0a)
-    #         p0b=list(popt1)+[10]
-    #         popt2, pcov = curve_fit(func3, xdata, ydata,bounds = ([0,0,0,0,0,0],[120,120,200]+2*[np.inf]+[120]),p0=p0b)
-    #         residuals = ydata- func3(xdata, *popt2)
-    #         ss_res = np.sum(residuals**2)
-    #         ss_tot = np.sum((ydata-np.mean(ydata))**2)
-    #         r_squared1 = 1 - (ss_res / ss_tot)
-    #         popt3, pcov = curve_fit(func4, xdata, ydata,bounds = ([0,0,0,0,0,0],[120,120,200]+2*[np.inf]+[120]),p0=p0b)
-    #         residuals = ydata- func4(xdata, *popt3)
-    #         ss_res = np.sum(residuals**2)
-    #         ss_tot = np.sum((ydata-np.mean(ydata))**2)
-    #         r_squared2 = 1 - (ss_res / ss_tot)
-    #     #     ax.plot(xdata, func2(xdata, *popt1), 'r-')
-    #         if r_squared1>r_squared2:
-    #     #         ax.plot(xdata, func3(xdata, *popt2), 'b-')
-    #             popt=popt2
-    #         else:
-    #     #         ax.plot(xdata, func4(xdata, *popt3), 'b-')
-    #             popt=popt3
-    #         background = popt[3]
-    #     except RuntimeError:
-    #         print('failed')
-    #         background = np.mean(p)
-    # #     print(popt[3],popt0[3])
-    # #     width_pix = popt0[0]*popt0[2]
+
     background = np.mean(
         (np.mean(p[: width_factor // 6]), np.mean(p[-width_factor // 6 :]))
     )
@@ -177,23 +133,34 @@ def get_width_pixel(
     return a * np.sqrt(max(0, np.linalg.norm(point1 - point2) * (width_pix) / len(p)))
 
 
-def get_width_edge(edge, resolution, t, local=False, threshold_averaging=10):
-    pixel_conversion_factor = 1.725
+def get_width_edge(
+    edge: Edge, resolution: int, t: int, local=False, threshold_averaging=10
+) -> Dict[coord, float]:
+    """
+    Compute the width of the given edge for each point that is chosen on the hypha.
+    :param resolution: if resolution = 3 the width is computed every 3 pixel on the edge
+    :return: a dictionnary with the width for each point chosen on the hypha
+    """
+    pixel_conversion_factor = 1.725  # TODO(FK): use the designated function instead
     pixel_list = edge.pixel_list(t)
     pixels = []
     indexes = []
     source_images = []
     poss = []
     widths = {}
+    # Long edges
     if len(pixel_list) > 3 * resolution:
         for i in range(0, len(pixel_list) // resolution):
             index = i * resolution
             indexes.append(index)
             pixel = pixel_list[index]
             pixels.append(pixel)
-            source_img, pos = get_source_image(edge.experiment, pixel, t, local)
+            source_img, pos = get_source_image(
+                edge.experiment, pixel, t, local
+            )  # TODO(FK): very not efficient
             source_images.append(source_img)
             poss.append(pos)
+    # Small edges
     else:
         indexes = [0, len(pixel_list) // 2, len(pixel_list) - 1]
         for index in indexes:
@@ -249,3 +216,39 @@ def get_width_info(experiment, t, resolution=50, skip=False):
             # Maybe change to Nan if it doesnt break the rest
             edge_width[edge] = 40
     return edge_width
+
+
+if __name__ == "__main__":
+
+    from amftrack.util.sys import (
+        update_plate_info_local,
+        update_plate_info,
+        get_current_folders,
+        get_current_folders_local,
+        data_path,
+    )
+    import os
+    from random import choice
+
+    plate_name = "20220330_2357_Plate19"
+
+    directory = data_path + "/"
+    ## Set up experiment object
+    update_plate_info_local(directory)
+    # update_plate_info(data_path)
+    folder_df = get_current_folders_local(directory)
+    selected_df = folder_df.loc[folder_df["folder"] == plate_name]
+    i = 0
+    plate = 19
+    folder_list = list(selected_df["folder"])
+    directory_name = folder_list[i]
+    exp = Experiment(plate, directory)
+    exp.load(selected_df.loc[selected_df["folder"] == directory_name], labeled=False)
+
+    ## Select a random Edge at time 0
+    (G, pos) = exp.nx_graph[0], exp.positions[0]
+    edge = choice(list(G.edges))
+    edge_exp = Edge(Node(edge[0], exp), Node(edge[1], exp), exp)
+
+    ## Run the width function
+    print(get_width_edge(edge_exp, resolution=3, t=0))
