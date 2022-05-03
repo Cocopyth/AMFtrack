dc8fba7625076f1a47e77e3cb7685608b5b83964
Félix Kahane <felix@kahane.fr>
2022-05-03 11:25:55 +0200

    Add crop util

--- test/util/test_plot.py
+++ test/util/test_plot.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
-from amftrack.util.plot import pixel_list_to_matrix
+from numpy.testing import assert_array_equal
+from amftrack.util.plot import pixel_list_to_matrix, crop_image
 
 
 class TestPlot(unittest.TestCase):
@@ -21,4 +22,30 @@ class TestPlot(unittest.TestCase):
         self.assertTrue((M == M_).all())
         self.assertSequenceEqual(
             pixel_list_to_matrix(pixel_list, margin=1).shape, (6, 8)
+        )
+
+    def test_crop_image(self):
+        a = np.array(
+            [
+                [1.0, 2.3, 12.0, 1.0],
+                [4.5, 2.3, 0.0, 12.0],
+                [2.3, 0.3, 5.5, 8.9],
+                [2.3, 0.3, 5.5, 8.9],
+                [2.3, 0.3, 5.5, 8.9],
+            ]
+        )
+
+        assert_array_equal(
+            crop_image(a, [[0, 0], [3, 2]]),
+            np.array([[1.0, 2.3, 12.0], [4.5, 2.3, 0.0]]),
+        )
+
+        assert_array_equal(
+            crop_image(a, [[0, 0], [10, 10]]),
+            a,
+        )
+
+        assert_array_equal(
+            crop_image(a, [[-1, -1], [2, 2]]),
+            np.array([[1.0, 2.3], [4.5, 2.3]]),
         )
