152e18862a8a2c48b2c4c78e9b1b2bac98455c26
FelixKahane <felix.kahane@gmail.com>
2022-05-17 18:04:54 +0200

    Add plot callback

---
+++ amftrack/ml/callbacks.py
@@ -0,0 +1,22 @@
+import numpy as np
+import os
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from amftrack.ml.width.build_features import get_sets
+from amftrack.util.sys import storage_path
+
+
+class SavePlots(keras.callbacks.Callback):
+    """This callback plots the curve for the training and saves them in the chosen directory"""
+
+    def __init__(self, directory_path):
+        super(SavePlots, self).__init__()
+        self.directory_path = directory_path
+
+    def on_train_end(self, logs=None):
+        history = self.model.history
+        for key in history.history.keys():
+            plt.plot(history.history[key])
+            plt.savefig(os.path.join(self.directory_path, key))
+            plt.close()
