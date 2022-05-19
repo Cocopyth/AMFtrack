0382392f20aa30dc8770760bf95d775011b562a3
FelixKahane <felix.kahane@gmail.com>
2022-05-19 09:13:30 +0200

    Fix data augmentation

--- amftrack/ml/width/data_augmentation.py
+++ amftrack/ml/width/data_augmentation.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 
 
+@tf.function
 def random_invert_slice(x, p=0.5):
     """Inversing blacks with whites"""
     if tf.random.uniform([]) < p:
@@ -10,19 +11,33 @@ def random_invert_slice(x, p=0.5):
     return x
 
 
-def random_invert(factor=0.5) -> tf.keras.layers.Layer:
-    return tf.keras.layers.Lambda(lambda x: random_invert_slice(x, factor))
+def random_invert(p=0.5) -> tf.keras.layers.Layer:
+    return tf.keras.layers.Lambda(lambda x: random_invert_slice(x, p))
 
 
+@tf.function
 def random_mirror_slice(x, p=0.5):
-    """Mirror transformation with respect to the centered vertical"""
+    """
+    Mirror transformation along the last axis with respect to the centered vertical
+    WARNING: Careful, this transformation doesn't work if the input shape is (None, 120, 1) (wrong axis)
+    """
     if tf.random.uniform([]) < p:
         x = tf.reverse(x, axis=(-1,))
     return x
 
 
-def random_mirror(factor=0.5) -> tf.keras.layers.Layer:
-    return tf.keras.layers.Lambda(lambda x: random_mirror_slice(x, factor))
+def random_mirror(p=0.5) -> tf.keras.layers.Layer:
+    return tf.keras.layers.Lambda(lambda x: random_mirror_slice(x, p))
+
+
+
+data_augmentation = tf.keras.Sequential(
+    [
+        tf.keras.Input(shape=(120,)),  # TODO(FK): change here the shape
+        random_invert(p=0.5),
+        random_mirror(p=0.5),
+    ]
+)
 
 
 if __name__ == "__main__":
--- amftrack/ml/width/train_model.py
+++ amftrack/ml/width/train_model.py
@@ -41,30 +41,6 @@ callbacks = [
     plot_callback,
 ]
 
-### DATA TRANSFORMATIONS ###
-def random_invert_slice(x, p=0.5):
-    """Inversing blacks with whites"""
-    if tf.random.uniform([]) < p:
-        x = 255 - x
-    else:
-        x
-    return x
-
-
-def random_invert(factor=0.5) -> tf.keras.layers.Layer:
-    return tf.layers.Lambda(lambda x: random_invert_slice(x, factor))
-
-
-def random_mirror_slice(x, p=0.5):
-    """Mirror transformation with respect to the centered vertical"""
-    if tf.random.uniform([]) < p:
-        x = tf.reverse(x, axis=(0,))
-    return x
-
-
-def random_mirror(factor=0.5) -> tf.keras.layers.Layer:
-    return tf.layers.Lambda(lambda x: random_mirror_slice(x, factor))
-
 
 def main():
     # Get the model
@@ -108,10 +84,24 @@ def main():
 
 if __name__ == "__main__":
     # main()
-    # train, valid, test = get_sets(os.path.join(storage_path, "width3", "dataset_2"))
+    train, valid, test = get_sets(os.path.join(storage_path, "width3", "dataset_2"))
     # get_intel_on_dataset(train)
     # get_intel_on_dataset(valid)
     # get_intel_on_dataset(test)
+    from amftrack.ml.width.data_augmentation import data_augmentation
+    from amftrack.ml.util import display
+    from amftrack.ml.width.data_augmentation import random_mirror
+
+    stop = True
+
+    aug_train = train.map(
+        lambda x, y: (data_augmentation(x, training=True), y)
+    )  # TODO(FK) training = True?
+
+    stop = True
+
+    layer = random_mirror()
+    new = train.map(lambda x, y: (layer(x), y))
 
     a = tf.constant([[2.3], [3.2], [4.5], [12.2], [0], [0.0], [1.0]])
     b = tf.constant([2.0, 3.0, 4.3, 1.0, 1.0, 1.0, 1.0, 1.0])
