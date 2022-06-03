69437b89a3b7e98d105fd573469a3ec96ee8cca6
coco <corentin@bisot.com>
2022-06-03 10:09:45 +0200

    fixed rapid version of node tracking

--- amftrack/pipeline/scripts/mprun_demo.py
+++ amftrack/pipeline/scripts/mprun_demo.py
@@ -8,7 +8,7 @@ def test():
     import ast
     from scipy import sparse
     from datetime import datetime
-    from amftrack.pipeline.functions.image_processing.node_id import orient
+    from amftrack.pipeline.functions.image_processing.experiment_util import orient
     import scipy.io as sio
     import cv2
     import imageio
