49ce6ed161806daf0f95529f079f4433f4e28735
coco <corentin@bisot.com>
2022-06-27 08:10:53 +0200

    - upgraded post_processer_1 for upload of analysis folders

---
+++ amftrack/pipeline/launching/launcher_scripts/analysis_uploader.py
@@ -0,0 +1,18 @@
+
+from amftrack.pipeline.functions.post_processing.area_hulls import *
+from amftrack.util.dbx import upload_folder
+directory_targ = str(sys.argv[1])
+name_job = str(sys.argv[2])
+stage = int(sys.argv[3])
+plates = sys.argv[4:]
+update_analysis_info(directory_targ)
+analysis_info = get_analysis_info(directory_targ)
+analysis_folders = analysis_info.loc[analysis_info['unique_id'].isin(plates)]
+dir_drop = "DATA/PRINCE"
+
+for index, row in analysis_folders.iterrows():
+    folder = row['folder_analysis']
+    id_unique = row['id_unique']
+    path = os.path.join(directory,folder)
+    target_drop = f'/{dir_drop}/{id_unique}//{folder}'
+    upload_folder(path,target_drop)
