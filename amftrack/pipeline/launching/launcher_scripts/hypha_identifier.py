804e7678dd1bcecd94d61f372a7df68365af41ba
coco <corentin@bisot.com>
2022-06-28 20:41:42 +0200

    - limited hypha tracking to 100 timepoints

--- amftrack/pipeline/launching/launcher_scripts/hypha_identifier.py
+++ amftrack/pipeline/launching/launcher_scripts/hypha_identifier.py
@@ -18,7 +18,7 @@ folders = folders.loc[folders["/Analysis/nx_graph_pruned_labeled.p"]==True]
 args = [directory_targ]
 num_parallel = 32
 time = "8:00:00"
-limit = 1000
+limit = 100
 version = 1
 suffix = "_labeled"
 lim_considered = 3
