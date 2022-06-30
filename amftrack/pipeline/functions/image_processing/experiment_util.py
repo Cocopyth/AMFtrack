c5215ecb87c6709f9ee9746ce053bee951430e67
Félix Kahane <felix@kahane.fr>
2022-06-30 10:45:02 +0200

    First working version of plot_full

--- amftrack/pipeline/functions/image_processing/experiment_util.py
+++ amftrack/pipeline/functions/image_processing/experiment_util.py
@@ -336,6 +336,121 @@ def plot_full_image_with_features(
         plt.show()
 
 
+def plot_full(
+    exp: Experiment,
+    t: int,
+    region=None,
+    edges: List[Edge] = [],
+    points: List[coord_int] = [],
+    segments: List[List[coord_int]] = [],
+    nodes: List[Node] = [],
+    downsizing=5,
+    dilation=1,
+    save_path="",
+    prettify=False,
+) -> None:
+    """
+    This is the general purpose function to plot the full image or a region `region` of the image at
+    any given timestep t. The region being specified in the GENERAL coordinates.
+    The image can be downsized by a chosen factor `downsized` with additionnal features such as: edges, nodes, points, segments.
+    The coordinates for all the objects are provided in the GENERAL referential.
+
+    :param region: choosen region in the full image, such as [[100, 100], [2000,2000]], if None the full image is shown
+    :param edges: list of edges to plot, it is the pixel list that is plotted, not a straight line
+    :param nodes: list of nodes to plot (only nodes in the `region` will be shown)
+    :param points: points such as [123, 234] to plot with a red cross on the image
+    :param segments: plot lines between two points that are provided
+    :param downsizing: factor by which we reduce the image resolution (5 -> image 25 times lighter)
+    :param dilation: only for edges: thickness of the edges (dilation applied to the pixel list)
+    :param save_path: full path to the location where the plot will be saved
+    :param prettify: if True, the image will be enhanced by smoothing the intersections between images
+
+    NB: the full region of a full image is typically [[0, 0], [26000, 52000]]
+    NB: the interesting region of a full image is typically [[12000, 15000], [26000, 35000]]
+    NB: the colors are chosen randomly for edges
+    NB: giving a smaller region greatly increase computation time
+    """
+
+    # TODO(FK): fetch image size from experiment object here, and use it in reconstruct image
+    # TODO(FK): colors for edges are not consistent
+    # NB: possible other parameters that could be added: alpha between layers, colors for object, figure_size
+
+    if region == None:
+        # Full image
+        image_coodinates = exp.image_coordinates[t]
+        region = get_bounding_box(image_coodinates)
+        region[1][0] += DIM_X  # TODO(FK): Shouldn't be hardcoded
+        region[1][1] += DIM_Y
+
+    # 1/ Image layer
+    im, f = reconstruct_image_from_general(
+        exp,
+        t,
+        downsizing=downsizing,
+        region=region,
+        prettify=prettify,
+        white_background=False,  # TODO(FK): add image dimention here dimx = ..
+    )
+    f_int = lambda c: f(c).astype(int)
+    new_region = [
+        f_int(region[0]),
+        f_int(region[1]),
+    ]  # should be [[0, 0], [d_x/downsized, d_y/downsized]]
+
+    # 2/ Edges layer
+    skel_im, _ = reconstruct_skeletton(
+        [edge.pixel_list(t) for edge in edges],
+        region=region,
+        color_seeds=None,  # Give color seed here
+        downsizing=downsizing,
+        dilation=dilation,
+    )
+
+    # 3/ Fusing layers
+    fig = plt.figure(
+        figsize=(12, 8)
+    )  # width: 30 cm height: 20 cm # TODO(FK): change dpi
+    ax = fig.add_subplot(111)
+    ax.imshow(im, cmap="gray", interpolation="none")
+    ax.imshow(skel_im, alpha=0.5, interpolation="none")
+
+    # 3/ Plotting the Nodes
+    size = 5
+    bbox_props = dict(boxstyle="circle", fc="white")
+    for node in nodes:
+        c = f(list(node.pos(t)))
+        if is_in_bounding_box(c, new_region):
+            node_text = ax.text(
+                c[1],
+                c[0],
+                str(node.label),
+                ha="center",
+                va="center",
+                size=size,
+                bbox=bbox_props,
+            )
+    # 4/ Plotting coordinates
+    points = [f(c) for c in points]
+    for c in points:
+        if is_in_bounding_box(c, new_region):
+            plt.plot(c[1], c[0], marker="x", color="red")
+
+    # 5/ Plotting segments
+    segments = [[f(segment[0]), f(segment[1])] for segment in segments]
+    for s in segments:
+        plt.plot(
+            [s[0][1], s[1][1]],  # x1, x2
+            [s[0][0], s[1][0]],  # y1, y2
+            color="white",
+            linewidth=2,
+        )
+
+    if save_path:
+        plt.savefig(save_path)
+    else:
+        plt.show()
+
+
 def reconstruct_image_simple(
     exp: Experiment, t: int, downsizing=1, white_background=True
 ) -> np.array:
