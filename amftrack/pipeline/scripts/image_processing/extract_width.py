0d0b6e56622322660f97acda3a4b9db8bc76d328
Félix Kahane <felix@kahane.fr>
2022-08-19 10:51:56 +0200

    Add new width extraction

--- amftrack/pipeline/scripts/image_processing/extract_width.py
+++ amftrack/pipeline/scripts/image_processing/extract_width.py
@@ -15,7 +15,7 @@ resolution = eval(sys.argv[3])
 i = int(sys.argv[-1])
 op_id = int(sys.argv[-2])
 
-run_info = pd.read_json(f"{temp_path}/{op_id}.json",dtype = {'unique_id':str})
+run_info = pd.read_json(f"{temp_path}/{op_id}.json", dtype={"unique_id": str})
 # plate = list(run_info['PrincePos'])[i]
 # Sometime plate in param file is inconsistent with folder name...
 # plate = int(list(run_info['folder'])[i].split('_')[-1][5:])
@@ -30,7 +30,7 @@ path_snap = directory + directory_name
 suffix = "/Analysis/nx_graph_pruned.p"
 
 (G, pos) = exp.nx_graph[0], exp.positions[0]
-edge_test = get_width_info(exp, 0, resolution=resolution, skip=skip)
+edge_test = get_width_info_new(exp, 0, resolution=resolution, skip=skip)
 nx.set_edge_attributes(G, edge_test, "width")
 print(i, f"saving {path_snap}")
 pickle.dump((G, pos), open(f"{path_snap}/Analysis/nx_graph_pruned_width.p", "wb"))
