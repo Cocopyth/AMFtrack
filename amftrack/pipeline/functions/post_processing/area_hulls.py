- fixed density extraction, factor 2 in length, very important for paper. Needs further fixing.

amftrack/pipeline/functions/post_processing/area_hulls.py
@@ -84,7 +84,7 @@ def get_length_in_ring(hull1, hull2, t, exp):
     nodes = get_nodes_in_ring(hull1, hull2, t, exp)
     edges = {edge for node in nodes for edge in node.edges(t)}
     tot_length = np.sum(
-        [np.linalg.norm(edge.end.pos(t) - edge.begin.pos(t)) * 1.725 for edge in edges]
+        [np.linalg.norm(edge.end.pos(t) - edge.begin.pos(t)) * 1.725/2 for edge in edges]
     )
     return tot_length
 

amftrack/util/video_util.py
@@ -471,7 +471,7 @@ def make_images_betweenness_random(exp):
         vmax = -2.5
         vmin = -4.5
         plot_edge_color_value(exp, t,
-                              lambda edge: np.log10(edge.current_flow_betweeness(t)), cmap=cm.get_cmap("Reds", 100),
+                              lambda edge: np.log10(edge.current_flow_betweeness(t)), cmap=cm.get_cmap("viridis", 100),
                               v_min=vmin, v_max=vmax,
                               plot_cmap=True, show_background=False, dilation=10,
                               label_colorbar="log random walk edge betweenness centrality",