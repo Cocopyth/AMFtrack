a2ba3ffbe8cb42a7bf226ed3703bcaafe539966d
Steeldune <nighttagle@gmail.com>
2023-05-08 12:22:56 +0200

    Fixed up the flux scale files

--- amftrack/pipeline/scripts/flow_processing/flux_extract.py
+++ amftrack/pipeline/scripts/flow_processing/flux_extract.py
@@ -1,23 +1,22 @@
 from amftrack.pipeline.development.high_mag_videos.kymo_class import *
-import os
-import imageio.v2 as imageio
-import matplotlib.pyplot as plt
-import cv2
-from tqdm import tqdm
-
-from amftrack.pipeline.functions.image_processing.extract_graph import (
-    from_sparse_to_graph,
-    generate_nx_graph,
-    clean_degree_4,
-)
-import scipy
+# import os
+# import imageio.v2 as imageio
+# import matplotlib.pyplot as plt
+# import cv2
+# from tqdm import tqdm
+#
+# from amftrack.pipeline.functions.image_processing.extract_graph import (
+#     from_sparse_to_graph,
+#     generate_nx_graph,
+#     clean_degree_4,
+# )
+# import scipy
 import sys
 import pandas as pd
 from amftrack.util.sys import temp_path
 
-
 directory = str(sys.argv[1])
-test_len = str(sys.argv[2])
+GST_params = sys.argv[2:6]
 folders = str(sys.argv[3])
 i = int(sys.argv[-1])
 op_id = int(sys.argv[-2])
@@ -29,11 +28,11 @@ img_address = dataframe["address_total"]
 
 print(dataframe)
 
-test_video = Kymo_video_analysis(img_address, logging=True, vid_type=None, 
+test_video = Kymo_video_analysis(img_address, logging=True, vid_type=None,
                                  fps=None, binning=None, filter_step=80,
-                                seg_thresh=10, show_seg=False)
+                                 seg_thresh=10, show_seg=False)
 edge_list = test_video.edge_objects
-target_length = int(2.5*test_video.magnification)
+target_length = int(2.5 * test_video.magnification)
 
 test_video.plot_extraction_img(target_length=target_length, save_img=True)
 edge_objs = test_video.edge_objects
@@ -41,25 +40,23 @@ edge_objs = test_video.edge_objects
 print('\n To work with individual edges, here is a list of their indices:')
 for i, edge in enumerate(edge_list):
     print('edge {}, {}'.format(i, edge.edge_name))
-    
+
 bin_nr = 1
-img_seq    = np.arange(len(edge_objs[0].video_analysis.selection_file))
+img_seq = np.arange(len(edge_objs[0].video_analysis.selection_file))
 kymos = []
 
-
-
 for edge in edge_objs:
-    edge_pic   = edge.view_edge(img_frame=40 ,save_im=True, target_length = target_length)
-    edge_video = edge.view_edge(img_frame = img_seq, save_im=True, quality = 6, target_length=target_length)
+    edge_pic = edge.view_edge(img_frame=40, save_im=True, target_length=target_length)
+    edge_video = edge.view_edge(img_frame=img_seq, save_im=True, quality=6, target_length=target_length)
     space_res = edge.video_analysis.space_pixel_size
     time_res = edge.video_analysis.time_pixel_size
     video_kymos = edge.extract_multi_kymo(bin_nr, target_length=target_length, kymo_adj=False)
     kymos.append(video_kymos)
     imshow_extent = [0, edge.video_analysis.space_pixel_size * edge.kymos[0].shape[1],
-                                  edge.video_analysis.time_pixel_size * edge.kymos[0].shape[0], 0]
+                     edge.video_analysis.time_pixel_size * edge.kymos[0].shape[0], 0]
     kymos_lefts, kymos_rights = edge.fourier_kymo(bin_nr, save_im=True, save_array=True)
-    speeds, times = edge.test_GST(15, w_start=3, C_thresh=0.95, C_thresh_falloff = 0.001, blur_size = 5, preblur=True, speed_thresh=20, plots=False)    
-    net_trans = edge.extract_transport(noise_thresh=0.15, plots=False, save_im=True, save_flux_array=True, margin=5)    
-
+    speeds, times = edge.test_GST(15, w_start=3, C_thresh=0.95, C_thresh_falloff=0.001, blur_size=5, preblur=True,
+                                  speed_thresh=20, plots=False)
+    net_trans = edge.extract_transport(noise_thresh=0.15, plots=False, save_im=True, save_flux_array=True, margin=5)
 
 print(dataframe)
