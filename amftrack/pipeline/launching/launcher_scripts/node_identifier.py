68d4c1a8c2c5f7cec72421a8f8245f7706399668
coco <corentin@bisot.com>
2023-05-31 22:44:32 +0200

    - made make graph after node id less SBU costly

--- amftrack/pipeline/launching/launcher_scripts/node_identifier.py
+++ amftrack/pipeline/launching/launcher_scripts/node_identifier.py
@@ -40,16 +40,16 @@ for unique_id in plates:
         name_job=name_job,
     )
 
-time = "12:00:00"
+time = "6:00:00"
 run_parallel_all_time(
     "make_labeled_graphs.py",
     args,
     folders,
-    num_parallel,
+    32,
     time,
     "make_graphs",
-    cpus=128,
-    node="fat",
+    cpus=32,
+    node="thin",
     dependency=True,
     name_job=name_job,
 )
