0a724efd72f23dc1e63328282d78494f14f82148
coco <corentin@bisot.com>
2023-08-24 11:47:27 +0200

    -1st working version of visu toools

--- amftrack/pipeline/launching/launcher_scripts/post_processer_2.py
+++ amftrack/pipeline/launching/launcher_scripts/post_processer_2.py
@@ -44,7 +44,7 @@ list_args = [{}] * len(list_f)
 # list_args= [[500]]
 overwrite = True
 load_graphs = True
-num_parallel = 32
+num_parallel = 16
 time = "12:00:00"
 for index, row in analysis_folders.iterrows():
     folder = row["folder_analysis"]
