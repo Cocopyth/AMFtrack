58a205759c5b43481ee7df6dd1090e16ef669d52
coco <corentin@bisot.com>
2023-09-05 17:01:23 +0200

    - test

--- amftrack/pipeline/development/high_mag_videos/high_mag_videos_fun.py
+++ amftrack/pipeline/development/high_mag_videos/high_mag_videos_fun.py
@@ -13,16 +13,16 @@
 #     remove_component,
 #     remove_holes,
 # )
-import numpy as np
-from amftrack.pipeline.functions.image_processing.extract_width_fun import (
-    generate_pivot_indexes,
-    compute_section_coordinates,
-)
-from skimage.measure import profile_line
-from amftrack.pipeline.functions.image_processing.experiment_class_surf import orient
-from skimage.filters import frangi
-from skimage.morphology import skeletonize
-import itertools,operator
+# import numpy as np
+# from amftrack.pipeline.functions.image_processing.extract_width_fun import (
+#     generate_pivot_indexes,
+#     compute_section_coordinates,
+# )
+# from skimage.measure import profile_line
+# from amftrack.pipeline.functions.image_processing.experiment_class_surf import orient
+# from skimage.filters import frangi
+# from skimage.morphology import skeletonize
+# import itertools,operator
 from scipy.ndimage.filters import generic_filter
 
 
