c2ce8764cc89f8f72e458f8e02b87d65bc4e1a9b
Corentin Bisot <corentin@bisot.com>
2023-12-21 15:05:11 +0100

    deleted unnecessary folders

--- build/lib/amftrack/util/aliases.py
+++
@@ -1,7 +0,0 @@
-from typing import Dict, Tuple
-import numpy as np
-
-node_coord_dict = Dict[str, Tuple[int, int]]
-binary_image = np.array  # boolean array of dimension 2
-coord = Tuple[float, float]
-coord_int = Tuple[int, int]
