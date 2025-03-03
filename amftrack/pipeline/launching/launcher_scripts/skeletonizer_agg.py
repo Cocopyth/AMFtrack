6945a94099f669c1307549daf7dce7674b2c35e5
coco <corentin@bisot.com>
2025-03-03 10:34:17 +0100

    fixed ML skelet

--- amftrack/pipeline/launching/launcher_scripts/skeletonizer_agg.py
+++ amftrack/pipeline/launching/launcher_scripts/skeletonizer_agg.py
@@ -18,7 +18,7 @@ all_folders = get_current_folders(
 )
 folders = all_folders.loc[all_folders["unique_id"].isin(plates)]
 folders = folders.loc[folders["/Img/TileConfiguration.txt.registered"] == True]
-num_parallel = 100
+num_parallel = 50
 time = "12:00:00"
 
 args = [directory_targ]
